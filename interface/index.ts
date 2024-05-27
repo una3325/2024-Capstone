f58e4a088c088811376e7f3b8836fd8d6cd1db30
una3325 <a33256448@gmail.com>
2024-05-28 05:29:33 +0900

    상세 페이지 댓글 기능 설계

--- interface/index.ts
+++ interface/index.ts
@@ -21,6 +21,13 @@ export interface FilterLayoutProps {
   isShow: boolean
 }
 
+export interface LikeType {
+  id: number
+  roomId: number
+  userId: number
+  createdAt: string
+}
+
 export interface RoomType {
   id: number
   images: string[]
@@ -44,6 +51,7 @@ export interface RoomType {
   hasWifi: boolean
   hasBarbeque: boolean
   hasFreeParking: boolean
+  likes?: LikeType[]
 }
 
 interface Account {
